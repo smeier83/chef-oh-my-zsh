85decc880c8970dd721e3009e86502d362ecf24b
Thomas Layh <thomas.layh@aoemedia.de>
2012-09-05 14:19:13 +0200

    [Bugfix] add correct path to custom plugins to use the flow3 plugin

--- recipes/flow3.rb
+++ recipes/flow3.rb
@@ -8,11 +8,11 @@ node[:oh_my_zsh][:users].each do |zsh_user|
 		interpreter "bash"
 		user zsh_user
 		code <<-EOH
-		git clone https://github.com/sandstorm/oh-my-zsh-flow3-plugin.git /home/#{zsh_user}/.oh-my-zsh/plugins/flow3
-		sudo chown -R vagrant:www-data /home/#{zsh_user}/.oh-my-zsh/plugins/flow3
-		sudo chmod 777 /home/#{zsh_user}/.oh-my-zsh/plugins/flow3/helper-postprocess-cmdlist.php
+		git clone https://github.com/sandstorm/oh-my-zsh-flow3-plugin.git /home/#{zsh_user}/.oh-my-zsh/custom/plugins/flow3
+		sudo chown -R vagrant:www-data /home/#{zsh_user}/.oh-my-zsh/custom/plugins/flow3
+		sudo chmod 777 /home/#{zsh_user}/.oh-my-zsh/custom/plugins/flow3/helper-postprocess-cmdlist.php
 		EOH
-		not_if {File.exists?("/home/#{zsh_user}/.oh-my-zsh/plugins/flow3/helper-postprocess-cmdlist.php")}
+		not_if {File.exists?("/home/#{zsh_user}/.oh-my-zsh/custom/plugins/flow3/helper-postprocess-cmdlist.php")}
 	end
 
 end
