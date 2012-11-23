4b7a2f4246af351a73d54af72572d831d1bc51d8
Thomas Layh <thomas.layh@aoemedia.de>
2012-11-23 14:15:37 +0100

    remove unused sudo

--- recipes/flow3.rb
+++ recipes/flow3.rb
@@ -9,8 +9,8 @@ node[:oh_my_zsh][:users].each do |zsh_user|
 		user zsh_user
 		code <<-EOH
 		git clone https://github.com/sandstorm/oh-my-zsh-flow3-plugin.git /home/#{zsh_user}/.oh-my-zsh/custom/plugins/flow3
-		sudo chown -R vagrant:www-data /home/#{zsh_user}/.oh-my-zsh/custom/plugins/flow3
-		sudo chmod 777 /home/#{zsh_user}/.oh-my-zsh/custom/plugins/flow3/helper-postprocess-cmdlist.php
+		chown -R vagrant:www-data /home/#{zsh_user}/.oh-my-zsh/custom/plugins/flow3
+		chmod 777 /home/#{zsh_user}/.oh-my-zsh/custom/plugins/flow3/helper-postprocess-cmdlist.php
 		EOH
 		not_if {File.exists?("/home/#{zsh_user}/.oh-my-zsh/custom/plugins/flow3/helper-postprocess-cmdlist.php")}
 	end
