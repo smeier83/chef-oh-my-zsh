5486ee10a50c2d93691d0cc7524dedea3149b90f
smeier83 <info@stephan-meier.com>
2014-12-24 12:24:57 +0100

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -4,3 +4,4 @@ license          "MIT"
 description      "Installs/Configures oh-my-zsh"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.2"
+name             "Typo3 Flow"
